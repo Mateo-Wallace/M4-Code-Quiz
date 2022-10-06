e3030696b5d9d3cadbed1e97400e2ee69a92b876
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-06 07:10:49 -0400

    refactored questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,46 +8,41 @@ var questions = [
             { text: 'alerts', correct: true},
             { text: 'numbers', correct: false}
         ]
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within ___.',
+        choices: [
+            { text: 'quotes', correct: false},
+            { text: 'curly brackets', correct: false},
+            { text: 'parentheses', correct: true},
+            { text: 'square brackets', correct: false}
+        ]
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store ___.',
+        choices: [
+            { text: 'numbers and strings', correct: false},
+            { text: 'other arrays', correct: false},
+            { text: 'booleans', correct: false},
+            { text: 'all of the above', correct: true}
+        ],
+    },
+    {
+        title: 'String values must be enclosed within ___ when being assigned to variables.',
+        choices: [
+            { text: 'commas', correct: false},
+            { text: 'curly brackets', correct: false},
+            { text: 'quotes', correct: true},
+            { text: 'parentheses', correct: false}
+        ]
+    },
+    {
+        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: [
+            { text: 'JavaScript', correct: false},
+            { text: 'terminal / bash', correct: false},
+            { text: 'for loops', correct: true},
+            { text: 'console.log', correct: false}
+        ]
     }
-    // ,
-    // {
-    //     title: 'The condition in an if / else statement is enclosed within ___.',
-    //     choices: [
-    //         'quotes',
-    //         'curly brackets',
-    //         'parentheses',
-    //         'square brackets'
-    //     ],
-    //     answer: 'parentheses'
-    // },
-    // {
-    //     title: 'Arrays in JavaScript can be used to store ___.',
-    //     choices: [
-    //         'numbers and strings',
-    //         'other arrays',
-    //         'booleans',
-    //         'all of the above'
-    //     ],
-    //     answer: 'all of the above'
-    // },
-    // {
-    //     title: 'String values must be enclosed within ___ when being assigned to variables.',
-    //     choices: [
-    //         'commas',
-    //         'curly brackets',
-    //         'quotes',
-    //         'parentheses'
-    //     ],
-    //     answer: 'quotes'
-    // },
-    // {
-    //     title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-    //     choices: [
-    //         'JavaScript',
-    //         'terminal / bash',
-    //         'for loops',
-    //         'console.log'
-    //     ],
-    //     answer: 'console.log'
-    // }
 ];
