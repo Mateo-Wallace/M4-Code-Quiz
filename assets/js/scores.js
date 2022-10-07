259112c17e142752395520db6c2d3aa56354b8f9
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-10-06 21:31:28 -0400

    finished highscores page

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,20 +1,28 @@
 function printHighscores() {
     // either get scores from localstorage or set to empty array
-
+    var scores = JSON.parse(localStorage.getItem("savedScores"));
     // sort highscores by score property in descending order
-
-    // loop through scores
+    scores.sort( function ( a, b ) { return b.score - a.score; } );
     // create li tag for each high score
-
-    // display on page
+    for(var i=0;i<scores.length;i++) {
+        //create new button for each choice
+        var li = document.createElement("li");
+        li.classList.add('list-style')
+        li.textContent = scores[i].init + " - " + scores[i].score;
+        //display on the page
+        document.getElementById("highscores").appendChild(li);
+    }
 }
 
 /* FUNCTION TO CLEAR SCORES */
 function clearHighscores() {
-    console.log('clear button clicked')
     // remove an item from local storage
+    localStorage.removeItem("savedScores");
     // reload the page
+    window.location.reload();
 }
 
-document.getElementById('clear').addEventListener('click', clearHighscores)
+/* CLICK EVENT TO RUN THE CLEAR SCORES FUNCTION */
+document.querySelector("#clear").addEventListener("click", clearHighscores);
+// run function when page loads
 printHighscores();
